c273afe970366bab52166f4e75d6ecc9b8da491b
beagginn <abgfaustino@gmail.com>
2026-04-30 21:07:11 -0300

    tarefa08

---
+++ tarefa08/parse_cardapio.py
@@ -0,0 +1,36 @@
+from xml.dom.minidom import parse
+
+dom = parse("cardapio.xml")
+
+cardapio = dom.documentElement
+
+pratos = cardapio.getElementsByTagName('prato')
+
+id_prato = 0
+for prato in pratos:
+    id_prato += 1
+    nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
+    descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
+    preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
+    calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
+    print(f'{id_prato} - {nome}')
+
+id_lido = int(input("Digite o id do prato para saber mais: "))
+prato = pratos[id_lido-1]
+print("---\n")
+
+
+elemento_nome = prato.getElementsByTagName('nome')[0]
+nome = elemento_nome.firstChild.nodeValue
+elemento_descricao = prato.getElementsByTagName('descricao')[0]
+descricao = elemento_descricao.firstChild.nodeValue
+elemento_preco = prato.getElementsByTagName('preco')[0]
+preco = elemento_preco.firstChild.nodeValue
+elemento_calorias = prato.getElementsByTagName('calorias')[0]
+calorias = elemento_calorias.firstChild.nodeValue
+
+print("Nome:", nome)
+print("Descrição:", descricao)
+print("Preço:", preco)
+print("Calorias:", calorias)
+print("---\n")
